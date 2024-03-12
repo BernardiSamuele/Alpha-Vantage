9c117592887d96ca550dbaef94c6f8c777438f3e
BernardiSamuele <s.bernardi.2625@vallauri.edu>
2024-03-12 18:47:19 +0100

    Added keyword search

--- lib.js
+++ lib.js
@@ -1,19 +1,47 @@
 function getGlobalQuotes(symbol) {
     const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`
     $.getJSON(url,
-        function (data) {
+        (data) => {
             const listCompany = document.getElementById("listCompany")
             console.log(data)
-            const globalQuoteData = data["Global Quote"]
-            document.getElementById("companyField").textContent = listCompany.querySelectorAll("option")[listCompany.selectedIndex].textContent 
-            document.getElementById("symbolField").textContent = globalQuoteData["01. symbol"]
-            document.getElementById("openField").textContent = globalQuoteData["02. open"]
-            document.getElementById("highField").textContent = globalQuoteData["03. high"]
-            document.getElementById("lowField").textContent = globalQuoteData["04. low"]
-            // document.getElementById("").textContent = globalQuoteData["05. price"]
-            document.getElementById("volumeField").textContent = globalQuoteData["06. volume"]
-            document.getElementById("latestTraringDayField").textContent = globalQuoteData["07. latest trading day"]
-            document.getElementById("previousCloseField").textContent = globalQuoteData["08. previous close"]
-            document.getElementById("changeField").textContent = globalQuoteData["09. change"]
+            loadDataInTable(data["Global Quote"])
         })
+}
+
+function getSymbolAutocompletePrompts(text) {
+    if (text == "") {
+        return
+    }
+    const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${text}&apikey=${API_KEY}`
+    const _autocompleteList = document.getElementById("autocomplete-list")
+    _autocompleteList.innerHTML = ""
+    $.getJSON(url, (data) => {
+        console.log(data)
+        data["bestMatches"].forEach(element => {
+            const _div = document.createElement("div")
+            _div.innerHTML = `<b>${element["2. name"]}</b> - ${element["1. symbol"]}`
+            _div.symbol = element["1. symbol"]
+            _div.company = element["2. name"]
+            _autocompleteList.append(_div)
+            _div.addEventListener("click", function () {
+                getGlobalQuotes(this.symbol)
+                document.getElementById("txtAutocomplete").innerHTML = this.company
+                document.getElementById("autocomplete-list").innerHTML = ""
+            })
+        })
+    })
+}
+
+function loadDataInTable(globalQuoteData) {
+    console.log("Global Quote Data", globalQuoteData)
+    document.getElementById("companyField").textContent = listCompany.querySelectorAll("option")[listCompany.selectedIndex].textContent
+    document.getElementById("symbolField").textContent = globalQuoteData["01. symbol"]
+    document.getElementById("openField").textContent = globalQuoteData["02. open"]
+    document.getElementById("highField").textContent = globalQuoteData["03. high"]
+    document.getElementById("lowField").textContent = globalQuoteData["04. low"]
+    // document.getElementById("").textContent = globalQuoteData["05. price"]
+    document.getElementById("volumeField").textContent = globalQuoteData["06. volume"]
+    document.getElementById("latestTraringDayField").textContent = globalQuoteData["07. latest trading day"]
+    document.getElementById("previousCloseField").textContent = globalQuoteData["08. previous close"]
+    document.getElementById("changeField").textContent = globalQuoteData["09. change"]
 }
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const API_KEY = "3GFAI4VS0CGCGTJY"
+const DEMO_API_KEY = "demo"
 
 window.onload = () => {
     jQuery.noConflict();
-    document.getElementById("listCompany").addEventListener("input", function () {
+    document.getElementById("listCompany").addEventListener("change", function () {
         getGlobalQuotes(this.value)
+    })
+    document.getElementById("txtAutocomplete").addEventListener("input", function () {
+        getSymbolAutocompletePrompts(this.value)
     })
 
     getGlobalQuotes(document.getElementById("listCompany").value)
