f67780a275b28b9863370975dd158f1a34387152
BernardiSamuele <s.bernardi.2625@vallauri.edu>
2024-03-13 15:50:21 +0100

    Solved a bug with requests and implemented error handling

--- lib.js
+++ lib.js
@@ -1,9 +1,17 @@
+let currentCompany = ""
+
 function getGlobalQuotes(symbol) {
-    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`
+    const url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${DEMO_API_KEY}`
     $.getJSON(url,
         (data) => {
-            const listCompany = document.getElementById("listCompany")
             console.log(data)
+            console.log(url)
+            if(data["Global Quote"] == null){
+                document.querySelector(".errorDisplay").style.display = "block"
+                listCompany.selectedIndex = -1
+                return
+            }
+            document.querySelector(".errorDisplay").style.display = "none"
             loadDataInTable(data["Global Quote"])
         })
 }
@@ -12,11 +20,10 @@ function getSymbolAutocompletePrompts(text) {
     if (text == "") {
         return
     }
-    const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${text}&apikey=${API_KEY}`
+    const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${text}&apikey=${DEMO_API_KEY}`
     const _autocompleteList = document.getElementById("autocomplete-list")
     _autocompleteList.innerHTML = ""
     $.getJSON(url, (data) => {
-        console.log(data)
         data["bestMatches"].forEach(element => {
             const _div = document.createElement("div")
             _div.innerHTML = `<b>${element["2. name"]}</b> - ${element["1. symbol"]}`
@@ -24,8 +31,9 @@ function getSymbolAutocompletePrompts(text) {
             _div.company = element["2. name"]
             _autocompleteList.append(_div)
             _div.addEventListener("click", function () {
+                currentCompany = this.company
                 getGlobalQuotes(this.symbol)
-                document.getElementById("txtAutocomplete").innerHTML = this.company
+                document.getElementById("txtAutocomplete").value = this.company
                 document.getElementById("autocomplete-list").innerHTML = ""
             })
         })
@@ -33,13 +41,12 @@ function getSymbolAutocompletePrompts(text) {
 }
 
 function loadDataInTable(globalQuoteData) {
-    console.log("Global Quote Data", globalQuoteData)
-    document.getElementById("companyField").textContent = listCompany.querySelectorAll("option")[listCompany.selectedIndex].textContent
+    document.getElementById("companyField").textContent = currentCompany
     document.getElementById("symbolField").textContent = globalQuoteData["01. symbol"]
     document.getElementById("openField").textContent = globalQuoteData["02. open"]
     document.getElementById("highField").textContent = globalQuoteData["03. high"]
     document.getElementById("lowField").textContent = globalQuoteData["04. low"]
-    // document.getElementById("").textContent = globalQuoteData["05. price"]
+    document.getElementById("priceField").textContent = globalQuoteData["05. price"]
     document.getElementById("volumeField").textContent = globalQuoteData["06. volume"]
     document.getElementById("latestTraringDayField").textContent = globalQuoteData["07. latest trading day"]
     document.getElementById("previousCloseField").textContent = globalQuoteData["08. previous close"]
--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const DEMO_API_KEY = "demo"
 window.onload = () => {
     jQuery.noConflict();
     document.getElementById("listCompany").addEventListener("change", function () {
+        currentCompany = this.querySelectorAll("option")[this.selectedIndex].textContent
         getGlobalQuotes(this.value)
     })
     document.getElementById("txtAutocomplete").addEventListener("input", function () {
         getSymbolAutocompletePrompts(this.value)
     })
 
+    const listCompany = document.getElementById("listCompany")
+    currentCompany = listCompany.querySelectorAll("option")[listCompany.selectedIndex].textContent
     getGlobalQuotes(document.getElementById("listCompany").value)
 }
